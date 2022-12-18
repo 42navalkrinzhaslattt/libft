a2a283db9abb2c57461e97b9a14f8c9c376b8815
42navalkrinzhaslattt <120563003+42navalkrinzhaslattt@users.noreply.github.com>
2022-12-18 18:42:22 +0100

    calloc

---
+++ ft_calloc/ft_calloc.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void    *ft_calloc(size_t nmemb, size_t size)
+{
+    void    *arr;
+
+    arr = (void *)malloc(nmemb * size);
+    if (!arr || nmemb * size == 0)
+        return (NULL);
+    ft_bzero(arr, nmemb * size);
+    return (arr);
+}
--- libft.h
+++ libft.h
@@ -21,6 +21,7 @@ size_t  ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char    *ft_strdup(const char *s);
 void    ft_bzero(void *s, size_t n);
+void    *ft_calloc(size_t nmemb, size_t size);
 void    *ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);

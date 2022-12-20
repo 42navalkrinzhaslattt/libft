6e01623c376a71ceaed14254c162950fe3bb6da2
42navalkrinzhaslattt <120563003+42navalkrinzhaslattt@users.noreply.github.com>
2022-12-20 11:31:30 +0100

    file descriptor functions

--- libft.h
+++ libft.h
@@ -3,7 +3,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-//int     ft_isprint(char c);
+int     ft_isprint(char c);
 int     ft_atoi(const char *nptr);
 int     ft_isalnum(char c);
 int     ft_isalpha(char c);
@@ -33,5 +33,9 @@ void    *ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
+void    ft_putchar_fd(char c, int fd);
+void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
 
 #endif
